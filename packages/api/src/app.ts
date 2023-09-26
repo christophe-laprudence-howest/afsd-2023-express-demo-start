ac01552ff68b91234379b4eafea716eeaeb1b495
Christophe Laprudence <christophe.laprudence@howest.be>
2023-09-26 11:17:49 +0200

    feature: all routes (methods)

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -13,6 +13,25 @@ app.get('/', (request: Request, response: Response) => {
   response.send(`Welcome, just know: you matter!`)
 })
 
+// temp_store
+const shopList = ['Maxi Zoo', 'Zooplius', 'Tom & Co']
+
+app.get('/shop', (req: Request, res: Response) => {
+  res.send(shopList)
+})
+
+app.get('/shop/:myShopId', (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.myShopId)
+  console.log(`id: ${id}`)
+  res.send({ shop: shopList[id] })
+})
+
+app.post('/shop', (req: Request, res: Response) => {
+  const shop = req.body
+  const shopName = shop.name
+  shopList.push(shopName)
+  res.send({ shop: shopName })
+})
 
 // APP START
 app.listen(port, () => {
